7e760004e08262a1403cbeaf2f3a6ae04681714f
Kodpencerem <emrullah04@outlook.com>
2023-06-07 09:27:43 +0300

    Add project files.

---
+++ CalculatorService/CalculatorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalculatorService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalculatorService" in both code and config file together.
+    public class CalculatorService : ICalculatorService
+    {
+        public int Divide(int Numerator, int Denominator)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

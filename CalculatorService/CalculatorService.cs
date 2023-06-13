7d784f6662b1da69d28d62615fd16b095983533a
Kodpencerem <emrullah04@outlook.com>
2023-06-13 15:58:16 +0300

    Fault Exception

--- CalculatorService/CalculatorService.cs
+++ CalculatorService/CalculatorService.cs
@@ -1,4 +1,5 @@
-﻿using System.ServiceModel;
+﻿using System;
+using System.ServiceModel;
 
 namespace CalculatorService
 {
@@ -9,7 +10,11 @@ namespace CalculatorService
     {
         public float Divide(float Numerator, float Denominator)
         {
-            return Numerator/Denominator;
+            if (Denominator == 0)
+            {
+                throw new FaultException("Denominator Can't Be Zero", new FaultCode("DivideByZeroFault"));
+            }
+            return Numerator / Denominator;
         }
     }
 }
